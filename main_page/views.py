c5c23206b3c431b256f3827650b75d6ae4255df5
Costello90 <alextasssss@gmail.com>
2022-12-18 05:56:32 +0200

    Decomposition index.html

--- main_page/views.py
+++ main_page/views.py
@@ -1,5 +1,6 @@
+from datetime import datetime
 from django.shortcuts import render
-from .models import About, WhyUsCard, DishCategory, Dish, Events
+from .models import About, WhyUsCard, DishCategory, Dish, Events, RestaurantPhoto, Chefs
 
 
 # Create your views here.
@@ -9,12 +10,16 @@ def main_page(request):
     categories = DishCategory.objects.filter(is_visible=True)
     dishes = Dish.objects.filter(is_visible=True, is_special=False)
     specials_dishes = Dish.objects.filter(is_visible=True, is_special=True)
-    events = Events.objects.filter(is_visible=True)
+    events = Events.objects.filter(is_visible=True, date_stop_event__gt=datetime.now())
+    gallery = RestaurantPhoto.objects.filter(is_visible=True)
+    chefs = Chefs.objects.filter(is_visible=True)
     return render(request, 'main_page.html', context={
         'about': about,
         'cards': cards,
         'categories': categories,
         'dishes': dishes,
         'specials': specials_dishes,
-        'events': events
+        'events': events,
+        'gallery': gallery,
+        'chefss': chefs,
     })
