e9a16383502d45b43e56a50809aed32645658bd9
Costello90 <alextasssss@gmail.com>
2022-12-28 08:31:22 +0200

    Add google map in html

--- test_project_django_1_cafe/settings.py
+++ test_project_django_1_cafe/settings.py
@@ -11,24 +11,29 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os.path
 from pathlib import Path
+from dotenv import load_dotenv
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+dotenv_path = os.path.join('.', '.env')
+if os.path.exists(dotenv_path):
+    load_dotenv(dotenv_path)
+
 SECRET_KEY = 'django-insecure-5iys0*^t)g5c=85ppvsyy+vg4q#)%m04-mf6qguult!ofve7!l'
-GOOGLE_MAPS_API_KEY = ''
+# SECRET_KEY = os.environ.get('SECRET_KEY')
+GOOGLE_MAPS_API_KEY = os.environ.get('GOOGLE_MAPS_API_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -75,7 +80,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'test_project_django_1_cafe.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
@@ -85,7 +89,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -105,7 +108,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
@@ -117,13 +119,11 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
 
 # Extra places for collectstatic to fined static files.
 
